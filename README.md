f23b50c92c14d141e6f0471db7414be1beace526
Josh Chorlton <jchorlton@gmail.com>
2018-01-10 21:10:47 -0500

    Retrieval of public key is very simplified

--- README.md
+++ README.md
@@ -45,3 +45,32 @@ Parse using python:
 python-pcapng package is only python2
 docker run -it --rm -v "$PWD":/work -w /work python:2.7 bash
 pip install python-pcapng
+
+Match that outputted js against the ccid spec and figure out which calls are necessary
+
+Got stuck figuring out apdu/tpdu/etc.
+docker run -it --rm --privileged jac/gpg bash
+pcscd --foreground --debug --apdu --color | tee log.txt (https://ludovicrousseau.blogspot.ca/2011/07/pcscd-debug-output.html)
+less -R log.txt
+Found the call: APDU: 00 A4 00 0C 02 3F 00
+so its apdu
+
+Then I had to figure out how to get the public key
+So I hit up the apdu spec: http://cardwerk.com/smart-card-standard-iso7816-4-section-6-basic-interindustry-commands/
+The command of interest to get the public key (just the apdu): 00 CA 5F 50 00
+This is a GET DATA request according to the spec
+P1-P2 are 5F 50
+From the spec, "When the value of P1-P2 lies in the range from '4000' to 'FFFF', the value of P1-P2 shall be a BER-TLV tag on two bytes. The values '4000' and 'FFFF' are RFU."
+Perfect, so I grab the BER-TLV spec and start decoding: http://cardwerk.com/iso7816-4-annex-d-use-of-basic-encoding-rules-asn-1/
+But that doesn't seem to be working at all, regardless of endianness
+So I go back to the drawing board: gpg2 --card-status
+In the results I see "URL of public key : <my public key url>"
+Great, let's check out the gpg2 src and search for "URL of public key"
+AH! "print_name (fp, "URL of public key : ", info.pubkey_url);"
+rg pubkey_url
+Nothing super useful :(
+Let's check OpenSC
+After a few greps, I grepped for URL and found "{ "URL",      "OPENPGP_URL",     "3F00:5F50",      NULL              },"
+Wait, 5F50 looks very familiar, that was in the read command!!
+Googling brought me to the openpgp spec https://gnupg.org/ftp/specs/openpgp-card-1.1.pdf
+AH! 5F50 specifies to fetch the public URL. No BER-TLV after all (well not yet)
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-function usb() {
+function getPublicKey() {
     let device;
     navigator.usb.requestDevice({ filters: [{ vendorId: 0x1050 }] })
         .then(selectedDevice => {
@@ -10,170 +10,98 @@ function usb() {
         })
         .then(() => device.selectConfiguration(1))
         .then(() => device.claimInterface(2))
-.then(() => device.controlTransferIn({
-                    requestType: 'standard',
-                    recipient: 'device',
-                    request: 0x00,
-                    value: 0x0000,
-                    index: 0x0000}, 0))
-.then(resp => console.log(resp))
-.then(() => device.controlTransferIn({
-                    requestType: 'standard',
-                    recipient: 'device',
-                    request: 0x00,
-                    value: 0x0000,
-                    index: 0x0000}, 0))
-.then(resp => console.log(resp))
-.then(() => device.controlTransferIn({
-                    requestType: 'standard',
-                    recipient: 'device',
-                    request: 0x00,
-                    value: 0x0000,
-                    index: 0x0000}, 0))
-.then(resp => console.log(resp))
-.then(() => device.controlTransferIn({
-                    requestType: 'standard',
-                    recipient: 'device',
-                    request: 0x00,
-                    value: 0x0000,
-                    index: 0x0000}, 0))
-.then(resp => console.log(resp))
-.then(() => device.controlTransferIn({
-                    requestType: 'standard',
-                    recipient: 'device',
-                    request: 0x00,
-                    value: 0x0000,
-                    index: 0x0000}, 0))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x65, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])))
-.then(() => device.transferIn(0x02, 10))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x62, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00])))
-.then(() => device.transferIn(0x02, 28))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00])))
-.then(() => device.transferIn(0x02, 17))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x61, 0x07, 0x00, 0x00, 0x00, 0x00, 0x03, 0x01, 0x00, 0x00, 0x11, 0x10, 0x00, 0x15, 0x00, 0xFE, 0x00])))
-.then(() => device.transferIn(0x02, 10))
-.then(resp => console.log(resp))
-.then(() => device.controlTransferOut({
-                    requestType: 'class',
-                    recipient: 'interface',
-                    request: 0x01,
-                    value: 0x0300,
-                    index: 0x0002}, Uint8Array.from([])))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x65, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00])))
-.then(() => device.transferIn(0x02, 10))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x07, 0x00, 0x00, 0x00, 0x00, 0x05, 0x04, 0x00, 0x00, 0x00, 0xA4, 0x00, 0x0C, 0x02, 0x3F, 0x00])))
-.then(() => device.transferIn(0x02, 12))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x0B, 0x00, 0x00, 0x00, 0x00, 0x06, 0x04, 0x00, 0x00, 0x00, 0xA4, 0x04, 0x00, 0x06, 0xD2, 0x76, 0x00, 0x01, 0x24, 0x01])))
-.then(() => device.transferIn(0x02, 12))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x07, 0x04, 0x00, 0x00, 0x00, 0xCA, 0x00, 0x4F, 0x00])))
-.then(() => device.transferIn(0x02, 28))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x08, 0x04, 0x00, 0x00, 0x00, 0xCA, 0x5F, 0x52, 0x00])))
-.then(() => device.transferIn(0x02, 20))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x09, 0x04, 0x00, 0x00, 0x00, 0xCA, 0x00, 0xC4, 0x00])))
-.then(() => device.transferIn(0x02, 19))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x0A, 0x04, 0x00, 0x00, 0x00, 0xCA, 0x00, 0x6E, 0x00])))
-.then(() => device.transferIn(0x02, 236))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x0B, 0x04, 0x00, 0x00, 0x00, 0xCA, 0x7F, 0x74, 0x00])))
-.then(() => device.transferIn(0x02, 15))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x0C, 0x04, 0x00, 0x00, 0x00, 0xCA, 0x00, 0x5E, 0x00])))
-.then(() => device.transferIn(0x02, 12))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x0D, 0x04, 0x00, 0x00, 0x00, 0xCA, 0x00, 0x6E, 0x00])))
-.then(() => device.transferIn(0x02, 236))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x0E, 0x04, 0x00, 0x00, 0x00, 0xCA, 0x00, 0x6E, 0x00])))
-.then(() => device.transferIn(0x02, 236))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x0F, 0x04, 0x00, 0x00, 0x00, 0xCA, 0x00, 0x6E, 0x00])))
-.then(() => device.transferIn(0x02, 236))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x10, 0x04, 0x00, 0x00, 0x00, 0xCA, 0x00, 0x65, 0x00])))
-.then(() => device.transferIn(0x02, 23))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x11, 0x04, 0x00, 0x00, 0x00, 0xCA, 0x5F, 0x50, 0x00])))
-.then(() => device.transferIn(0x02, 91))
-.then(resp => {
-    let str = "";
-    for (let i = 10; i < 89; ++i) {
-        str += String.fromCharCode(resp.data.getUint8(i));
-    }
-    console.log(str);
-})
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x12, 0x04, 0x00, 0x00, 0x00, 0xCA, 0x00, 0x6E, 0x00])))
-.then(() => device.transferIn(0x02, 236))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x13, 0x04, 0x00, 0x00, 0x00, 0xCA, 0x00, 0xC4, 0x00])))
-.then(() => device.transferIn(0x02, 19))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x14, 0x04, 0x00, 0x00, 0x00, 0xCA, 0x00, 0x7A, 0x00])))
-.then(() => device.transferIn(0x02, 19))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x15, 0x04, 0x00, 0x00, 0x00, 0xCA, 0x01, 0x01, 0x00])))
-.then(() => device.transferIn(0x02, 12))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x16, 0x04, 0x00, 0x00, 0x00, 0xCA, 0x01, 0x02, 0x00])))
-.then(() => device.transferIn(0x02, 12))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x08, 0x00, 0x00, 0x00, 0x00, 0x17, 0x04, 0x00, 0x00, 0x00, 0x47, 0x81, 0x00, 0x02, 0xB6, 0x00, 0x00])))
-.then(() => device.transferIn(0x02, 266))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x18, 0x04, 0x00, 0x00, 0x00, 0xC0, 0x00, 0x00, 0xFF])))
-.then(() => device.transferIn(0x02, 267))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x19, 0x04, 0x00, 0x00, 0x00, 0xC0, 0x00, 0x00, 0x11])))
-.then(() => device.transferIn(0x02, 29))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x08, 0x00, 0x00, 0x00, 0x00, 0x1A, 0x04, 0x00, 0x00, 0x00, 0x47, 0x81, 0x00, 0x02, 0xB8, 0x00, 0x00])))
-.then(() => device.transferIn(0x02, 266))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x1B, 0x04, 0x00, 0x00, 0x00, 0xC0, 0x00, 0x00, 0x10])))
-.then(() => device.transferIn(0x02, 28))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x08, 0x00, 0x00, 0x00, 0x00, 0x1C, 0x04, 0x00, 0x00, 0x00, 0x47, 0x81, 0x00, 0x02, 0xA4, 0x00, 0x00])))
-.then(() => device.transferIn(0x02, 266))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x1D, 0x04, 0x00, 0x00, 0x00, 0xC0, 0x00, 0x00, 0xFF])))
-.then(() => device.transferIn(0x02, 267))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x1E, 0x04, 0x00, 0x00, 0x00, 0xC0, 0x00, 0x00, 0x11])))
-.then(() => device.transferIn(0x02, 29))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x65, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F, 0x00, 0x00, 0x00])))
-.then(() => device.transferIn(0x02, 10))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x65, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00])))
-.then(() => device.transferIn(0x02, 10))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x65, 0x00, 0x00, 0x00, 0x00, 0x00, 0x21, 0x00, 0x00, 0x00])))
-.then(() => device.transferIn(0x02, 10))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x65, 0x00, 0x00, 0x00, 0x00, 0x00, 0x22, 0x00, 0x00, 0x00])))
-.then(() => device.transferIn(0x02, 10))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x65, 0x00, 0x00, 0x00, 0x00, 0x00, 0x23, 0x00, 0x00, 0x00])))
-.then(() => device.transferIn(0x02, 10))
-.then(resp => console.log(resp))
-.then(() => device.transferOut(0x02, Uint8Array.from([0x65, 0x00, 0x00, 0x00, 0x00, 0x00, 0x24, 0x00, 0x00, 0x00])))
-.then(() => device.transferIn(0x02, 10))
-.then(resp => console.log(resp))
-    // .then(() => device.transferIn(0x82, 64))
-    // .then(() => device.transferIn(0x82, 155))
+
+        // icc power on
+        .then(() => device.transferOut(0x02, Uint8Array.from([0x62, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])))
+        .then(() => device.transferIn(0x02, 100))
+        .then(resp => {
+            // check that byte 8 is 00 meaning no error
+            if (resp.data.getUint8(7) !== 0) {
+                throw new Error('Unable to icc power on');
+            }
+        })
+
+        // get parameters
+        .then(() => device.transferOut(0x02, Uint8Array.from([0x6C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00])))
+        .then(() => device.transferIn(0x02, 100))
+        .then(resp => {
+            // check that byte 8 is 00 meaning no error
+            if (resp.data.getUint8(7) !== 0) {
+                throw new Error('Unable to get parameters');
+            }
+
+            // check for T-1 protocol
+            if (resp.data.getUint8(9) !== 1) {
+                throw new Error('Only T-1 protocol is supported');
+            }
+
+            // I don't know what the other fields mean so let's just assume its fine.
+        })
+
+        // set parameters (I have no idea what the parameters mean but whatever, it works)
+        // I think this sets protocol T=1
+        .then(() => device.transferOut(0x02, Uint8Array.from([0x61, 0x07, 0x00, 0x00, 0x00, 0x00, 0x02, 0x01, 0x00, 0x00, 0x11, 0x10, 0x00, 0x15, 0x00, 0xFE, 0x00])))
+        .then(() => device.transferIn(0x02, 100))
+        .then(resp => {
+            // check that byte 8 is 00 meaning no error
+            if (resp.data.getUint8(7) !== 0) {
+                throw new Error('Unable to get parameters');
+            }
+        })
+
+        // send an abort for whatever reason
+        .then(() => device.controlTransferOut({
+            requestType: 'class',
+            recipient: 'interface',
+            request: 0x01, // abort
+            value: 0x0200, // seq 2, slot 0
+            index: 0x0002 // interface 2
+        }, Uint8Array.from([])))
+
+        .then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x0B, 0x00, 0x00, 0x00, 0x00, 0x04, 0x04, 0x00, 0x00,
+            // adpu SELECT FILE
+            0x00, 0xA4,
+            // Direct selection by DF name
+            0x04,
+            // Select the first record
+            0x00,
+            // 6 bytes of data
+            0x06,
+            // This is selecting the AID directly by name
+            // defined in opensc in src/pkcs15init/openpgp.profile
+            0xD2, 0x76, 0x00, 0x01, 0x24, 0x01])))
+        .then(() => device.transferIn(0x02, 2065))
+        .then(resp => {
+            // check that byte 8 is 00 meaning no error
+            if (resp.data.getUint8(7) !== 0) {
+                throw new Error('Unable to get parameters');
+            }
+
+            // the data returned is 90 00
+            // this might just mean okay!
+        })
+        .then(() => device.transferOut(0x02, Uint8Array.from([0x6F, 0x05, 0x00, 0x00, 0x00, 0x00, 0x11, 0x04, 0x00, 0x00,
+            0x00,
+            // GET DATA
+            0xCA,
+            // URL of public key (https://gnupg.org/ftp/specs/openpgp-card-1.1.pdf)
+            // only the value is returned because it is a simple request
+            0x5F, 0x50,
+            0x00])))
+        .then(() => device.transferIn(0x02, 2065))
+        .then(resp => {
+            // the response should contain the public key
+            // abData starts at field 10
+            // and goes until the last two status bytes 90 00
+            let str = "";
+            for (let i = 10; i < resp.data.byteLength - 2; ++i) {
+                str += String.fromCharCode(resp.data.getUint8(i));
+            }
+            console.log(str);
+        })
         .catch(error => {
-            console.log(error);
+            alert('Error getting public key');
             debugger;
-            console.log(error)
         });
 }
 
@@ -181,7 +109,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <button onClick={usb}>USB</button>
+                <button onClick={getPublicKey}>USB</button>
             </div>
             );
     }
