a157c273df28e30974f4424e069cfb18512cf7d7
Josh Chorlton <jchorlton@gmail.com>
2018-03-03 20:15:10 -0500

    redirect from dashboard if user is not logged in

--- ui/src/components/Dashboard/index.js
+++ ui/src/components/Dashboard/index.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import PropTypes from 'prop-types';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import { fetchUserIfNeeded } from '../../actions/user';
 
-export default class Dashboard extends Component {
+class Dashboard extends Component {
+    static propTypes = {
+        fetchUserIfNeeded: PropTypes.func.isRequired,
+        history: PropTypes.shape({
+            push: PropTypes.func.isRequired,
+        }).isRequired,
+        user: ImmutablePropTypes.contains({
+            receivedAt: PropTypes.number,
+        }),
+    }
+
+    componentWillMount() {
+        const {
+            fetchUserIfNeeded,
+        } = this.props;
+
+        fetchUserIfNeeded();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const { history } = this.props;
+
+        if (nextProps.user.has('error')) {
+            history.push("/");
+        }
+    }
+
     render() {
-        // TODO redirect to home if not logged in
         return (
             <div className="dashboard">
                 Dashboard
@@ -10,3 +40,12 @@ export default class Dashboard extends Component {
             );
     }
 }
+
+export default withRouter(connect(
+    state => ({
+        user: state.user,
+    }),
+    dispatch => ({
+        fetchUserIfNeeded: () => dispatch(fetchUserIfNeeded()),
+    }),
+)(Dashboard));
