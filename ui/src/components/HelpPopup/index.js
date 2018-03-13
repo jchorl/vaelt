589c79d07307ce22888663969bad4c9fb0b5d55e
Josh Chorlton <jchorlton@gmail.com>
2018-03-13 17:12:15 -0400

    adding help popup

---
+++ ui/src/components/HelpPopup/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './helpPopup.css';
+
+export default class HelpPopup extends Component {
+    static propTypes = {
+        message: PropTypes.string.isRequired,
+    }
+
+    render() {
+        return (
+            <i className="fa fa-info-circle helpPopup">
+                <div className="message">
+                    { this.props.message }
+                </div>
+            </i>
+        );
+    }
+}
