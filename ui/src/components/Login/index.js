d6739ad88afaeb8442715d312bac83e7e9f07b67
Josh Chorlton <jchorlton@gmail.com>
2018-03-19 11:37:52 -0400

    remove TODO for centering login text

--- ui/src/components/Login/index.js
+++ ui/src/components/Login/index.js
@@ -49,7 +49,6 @@ class LoginRegister extends Component {
         this.setState({ isRegister: true });
     }
 
-    // TODO vertically venter welcome back
     render() {
         const { isRegister } = this.state;
         const { user } = this.props;
